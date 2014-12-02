5694e5b85e655158aaa3d36ea3c7280bbf0ce8c8
atzorvas <antonis@tzorvas.com>
2014-12-02 17:02:41 +0200

    add source files for "My Simple Calculator" firefoxOS intro project - by @giannisk

---
+++ 20141129-firefoxOS-intro/my_simple_calculator/js/buttons.js
@@ -0,0 +1,48 @@
+function run1(){
+	document.form.result.value += "1";}
+
+function run2(){
+	document.form.result.value += "2";}
+
+function run3(){
+	document.form.result.value += "3";}
+
+function run4(){
+	document.form.result.value += "4";}
+
+function run5(){
+	document.form.result.value += "5";}
+
+function run6(){
+	document.form.result.value += "6";}
+
+function run7(){
+	document.form.result.value += "7";}
+
+function run8(){
+	document.form.result.value += "8";}
+
+function run9(){
+	document.form.result.value += "9";}
+
+function run0(){
+	document.form.result.value += "0";}
+
+function add(){
+	document.form.result.value += "+";}
+
+function sub(){
+	document.form.result.value += "-";}
+
+function mul(){
+	document.form.result.value += "*";}
+
+function div(){
+	document.form.result.value += "/";}
+
+function eq(){
+	document.form.result.value += "=";}
+
+function result(){
+	var evaltemp = eval(document.form.result.value)
+	document.form.result.value = evaltemp;}
